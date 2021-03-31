748e188bd0dc5671f7dd5605bd6df6490d253551
kgurbani <77587686+kgurbani@users.noreply.github.com>
2021-03-31 11:44:45 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import logo from './icon1.jpg';
 import './App.css';
+import React from 'react';
+import ListItems from './ListItems'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faTrash} from '@fortawesome/free-solid-svg-icons'
+import {faPlusCircle} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+library.add(faTrash);
+library.add(faPlusCircle);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      items:[],
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+handleInput(e){
+  this.setState({
+    currentItem:{
+    text: e.target.value,
+    key: Date.now()
+    }
+  })
 }
+
+addItem(e){
+  e.preventDefault();
+  const newItem = this.state.currentItem;
+  console.log(newItem); 
+  if(newItem.text!==""){
+    const newItems = [...this.state.items,newItem];
+    this.setState({
+      items:newItems,
+      currentItem:{
+        text:'',
+        key:'',
+        completed: false
+      }
+    })
+  }
+}
+
+
+deleteItem(key){
+      const filteredItems = this.state.items.filter(item => item.key!==key);
+      this.setState({
+        items: filteredItems
+      })
+}
+
+handleClick = (e) => {
+  e.target.className.toggle("strikeThrough")
+}
+
+  render(){
+  return (
+    <div>
+      <img src={logo} width="70" height="70" className="logo"/>
+      <h2 className="app-title">TO DO APP</h2>
+      <div className="App">
+      <h3>ADD AN ITEM</h3>
+        <header>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter todo item"
+          value={this.state.currentItem.text}
+          onChange={this.handleInput}/>
+
+          
+          <button type="submit">ADD ITEM</button>
+        </form>
+      </header>
+      <ListItems items={this.state.items} 
+      handleClick={this.handleClick}
+      deleteItem = {this.deleteItem}>  
+      </ListItems>
+      </div>
+   </div>
+  );
+}}
+
 
 export default App;
